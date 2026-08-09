38131213c15eaab8d1d61cea59d7d587944ec73b
HL1057829 <leeho@gvsu.edu>
2026-08-09 16:06:39 -0400

    Increase accessory preload limit from 4 to 5

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ import { initDrag } from './drag.js';
 
   const loadedAccessories = await preloadAccessories(
   scene,
-  accessories.slice(0, 4),
+  accessories.slice(0, 5),
   baseSize,
   baseRadius,
   debug
