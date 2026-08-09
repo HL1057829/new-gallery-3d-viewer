6f2b2269caf6b671aace075bbe9fe49bf2abca32
HL1057829 <leeho@gvsu.edu>
2026-08-09 13:52:29 -0400

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
 
 export default defineConfig({
+  base: '/new-gallery-3d-viewer/',
   resolve: {
     alias: {
       os: resolve(__dirname, 'src/shims/os.js')
