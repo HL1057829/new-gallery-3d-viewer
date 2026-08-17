343abd43a2de1de9dd413dbf477b379df0da90b4
HL1057829 <leeho@gvsu.edu>
2026-08-17 13:39:46 -0400

    Load all accessories instead of the first three

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ import { initDrag } from './drag.js';
 
   const loadedAccessories = await preloadAccessories(
   scene,
-  accessories.slice(0, 3),
+  accessories,
   baseSize,
   baseRadius,
   debug
